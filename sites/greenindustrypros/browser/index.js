f71fe831d67a5f9d106ea9f4022cda33ef6a00ee
Jacob Bare <jacob@limit0.io>
2019-11-14 07:10:12 -0600

    Remove reveal-ad imports

--- sites/greenindustrypros/browser/index.js
+++ sites/greenindustrypros/browser/index.js
@@ -3,7 +3,6 @@ import DefaultTheme from '@base-cms/marko-web-theme-default/browser';
 import GTM from '@base-cms/marko-web-gtm/browser';
 import GAM from '@base-cms/marko-web-gam/browser';
 import GCSE from '@base-cms/marko-web-gcse/browser';
-import RevealAd from '@base-cms/marko-web-reveal-ad/browser';
 import Common from '@ac-business-media/package-common/browser';
 import Inquiry from '@ac-business-media/package-inquiry/browser';
 
@@ -11,7 +10,6 @@ DefaultTheme(Browser);
 GTM(Browser);
 GAM(Browser);
 GCSE(Browser);
-RevealAd(Browser);
 Common(Browser);
 Inquiry(Browser);
 
