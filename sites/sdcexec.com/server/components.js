0d8520245e46db540eee16ece7e42fbc9b28a7b5
Jacob Bare <jacob@limit0.io>
2019-12-03 13:15:16 -0600

    Handle infinite scroll using content list flow

--- sites/sdcexec.com/server/components.js
+++ sites/sdcexec.com/server/components.js
@@ -2,10 +2,12 @@ const ContentCardDeckFlow = require('./components/flows/content-card-deck');
 const ContentLoadMoreFlow = require('./components/flows/content-load-more');
 const ContentListFlow = require('./components/flows/content-list');
 const MagazineIssueArchiveFlow = require('./components/flows/magazine-issue-archive');
+const LatestContentListFlow = require('./components/flows/latest-content-list');
 
 module.exports = {
   ContentCardDeckFlow,
   ContentLoadMoreFlow,
   ContentListFlow,
   MagazineIssueArchiveFlow,
+  LatestContentListFlow,
 };
