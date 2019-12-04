c8e89072c5702546ea1f0413f9542165945ca766
Jacob Bare <jacob@limit0.io>
2019-12-04 07:06:24 -0600

    Remove search

--- sites/mixequipmentmag.com/browser/index.js
+++ sites/mixequipmentmag.com/browser/index.js
@@ -1,12 +1,10 @@
 import Browser from '@base-cms/marko-web/browser';
 import DefaultTheme from '@base-cms/marko-web-theme-default/browser';
 import GTM from '@base-cms/marko-web-gtm/browser';
-import GCSE from '@base-cms/marko-web-gcse/browser';
 import Common from '@ac-business-media/package-common/browser';
 
 DefaultTheme(Browser);
 GTM(Browser);
-GCSE(Browser);
 Common(Browser);
 
 export default Browser;
--- sites/mixequipmentmag.com/config/gcse.js
+++
@@ -1,5 +0,0 @@
-const GCSEConfiguration = require('@base-cms/marko-web-gcse/config');
-
-const config = new GCSEConfiguration('017383739850048358259:WMX-939532962');
-
-module.exports = config;
