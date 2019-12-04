41f3165def853411bf0c520d599528944786d78c
Jacob Bare <jacob@limit0.io>
2019-12-04 07:02:25 -0600

    Create initial site folder

---
+++ sites/mixequipmentmag.com/config/gcse.js
@@ -0,0 +1,5 @@
+const GCSEConfiguration = require('@base-cms/marko-web-gcse/config');
+
+const config = new GCSEConfiguration('017383739850048358259:WMX-939532962');
+
+module.exports = config;
---
+++ sites/mixequipmentmag.com/server/components.js
@@ -0,0 +1,13 @@
+const ContentCardDeckFlow = require('./components/flows/content-card-deck');
+const ContentLoadMoreFlow = require('./components/flows/content-load-more');
+const ContentListFlow = require('./components/flows/content-list');
+const MagazineIssueArchiveFlow = require('./components/flows/magazine-issue-archive');
+const LatestContentListFlow = require('./components/flows/latest-content-list');
+
+module.exports = {
+  ContentCardDeckFlow,
+  ContentLoadMoreFlow,
+  ContentListFlow,
+  MagazineIssueArchiveFlow,
+  LatestContentListFlow,
+};
