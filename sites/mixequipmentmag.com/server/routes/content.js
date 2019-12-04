37bcc865dcd1a91216d21283fd6c7d4f8e782098
Jacob Bare <jacob@limit0.io>
2019-12-04 07:07:47 -0600

    Remove company and whitepaper templates

--- sites/mixequipmentmag.com/server/routes/content.js
+++ sites/mixequipmentmag.com/server/routes/content.js
@@ -1,19 +1,9 @@
 const { withContent } = require('@base-cms/marko-web/middleware');
 const content = require('../templates/content');
 const contact = require('../templates/content/contact');
-const company = require('../templates/content/company');
-const whitepaper = require('../templates/content/whitepaper');
 const queryFragment = require('../graphql/fragments/content-page');
 
 module.exports = (app) => {
-  app.get('/*?whitepaper/:id(\\d{8})*', withContent({
-    template: whitepaper,
-    queryFragment,
-  }));
-  app.get('/*?company/:id(\\d{8})*', withContent({
-    template: company,
-    queryFragment,
-  }));
   app.get('/*?contact/:id(\\d{8})*', withContent({
     template: contact,
     queryFragment,
