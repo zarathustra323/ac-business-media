178f53b0ad4be4f67c846bf3beb549c72162171e
Jacob Bare <jacob@limit0.io>
2019-12-10 13:18:04 -0600

    Add magazine issue archive page + flow + node

--- sites/mixequipmentmag.com/server/components.js
+++ sites/mixequipmentmag.com/server/components.js
@@ -1,5 +1,7 @@
 const ContentCardDeckFlow = require('./components/flows/content-card-deck');
+const MagazineIssueArchiveFlow = require('./components/flows/magazine-issue-archive');
 
 module.exports = {
   ContentCardDeckFlow,
+  MagazineIssueArchiveFlow,
 };
--- sites/mixequipmentmag.com/server/routes/magazine.js
+++ sites/mixequipmentmag.com/server/routes/magazine.js
@@ -1,7 +1,7 @@
-// const { withMagazineIssue, withMagazinePublication } = require('@base-cms/marko-web/middleware');
+const { withMagazinePublication } = require('@base-cms/marko-web/middleware');
 const index = require('../templates/magazine');
-// const publication = require('../templates/magazine/publication');
-// const publicationFragment = require('../graphql/fragments/magazine-publication-page');
+const publication = require('../templates/magazine/publication');
+const publicationFragment = require('../graphql/fragments/magazine-publication-page');
 // const issue = require('../templates/magazine/issue');
 // const issueFragment = require('../graphql/fragments/magazine-issue-page');
 
@@ -10,10 +10,10 @@ module.exports = (app) => {
     res.marko(index);
   });
 
-  // app.get('/magazine/:id([a-fA-F0-9]{24})', withMagazinePublication({
-  //   template: publication,
-  //   queryFragment: publicationFragment,
-  // }));
+  app.get('/magazine/:id([a-fA-F0-9]{24})', withMagazinePublication({
+    template: publication,
+    queryFragment: publicationFragment,
+  }));
 
   // app.get('/magazine/:id(\\d+)', withMagazineIssue({
   //   template: issue,
