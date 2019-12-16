bcb027382b45f77b4038ff7c439791a145e5cd39
Jacob Bare <jacob@limit0.io>
2019-12-16 15:23:52 -0600

    Add magazine routes

--- sites/foodlogistics.com/server/routes/index.js
+++ sites/foodlogistics.com/server/routes/index.js
@@ -1,11 +1,15 @@
 const content = require('./content');
 const home = require('./home');
+const magazine = require('./magazine');
 const search = require('./search');
 const websiteSections = require('./website-section');
 
 module.exports = (app) => {
   // Homepage
   home(app);
+
+  // Magazine Pages
+  magazine(app);
 
   // Content Pages
   content(app);
