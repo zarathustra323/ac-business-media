577e7dcbcd78aee23baadf47481189d218935062
Jacob Bare <jacob@limit0.io>
2019-12-18 13:31:28 -0600

    Create horizontal related content block

---
+++ packages/refresh-theme/graphql/fragments/related-content-block.js
@@ -0,0 +1,28 @@
+const gql = require('graphql-tag');
+
+module.exports = gql`
+
+fragment WebsiteRelatedContentFragment on Content {
+  id
+  type
+  shortName
+  siteContext {
+    path
+  }
+  published
+  primaryImage {
+    id
+    src
+    alt
+    isLogo
+  }
+  ... on ContentWebinar {
+    startDate
+  }
+  ... on ContentEvent {
+    startDate
+    endDate
+  }
+}
+
+`;
