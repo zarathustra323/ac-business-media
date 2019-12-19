ca54d1386939df464c96aef04fa22adbb46ee05a
Jacob Bare <jacob@limit0.io>
2019-12-19 16:07:27 -0600

    Create optimized content latest list fragment

--- packages/refresh-theme/fragments.js
+++ packages/refresh-theme/fragments.js
@@ -1,7 +1,9 @@
 const contentList = require('./graphql/fragments/content-list');
+const contentLatest = require('./graphql/fragments/content-latest');
 const magazineIssueArchive = require('./graphql/fragments/magazine-issue-archive');
 
 module.exports = {
   contentList,
+  contentLatest,
   magazineIssueArchive,
 };
---
+++ packages/refresh-theme/graphql/fragments/content-latest.js
@@ -0,0 +1,60 @@
+const gql = require('graphql-tag');
+
+module.exports = gql`
+
+fragment WebsiteContentLatestFragment on Content {
+  id
+  type
+  shortName
+  teaser(input: { useFallback: false, maxLength: null })
+  siteContext {
+    path
+  }
+  published
+  company {
+    id
+    type
+    name
+    siteContext {
+      path
+    }
+  }
+  primaryImage {
+    id
+    src
+    alt
+    isLogo
+  }
+  ... on Authorable {
+    authors {
+      edges {
+        node {
+          id
+          name
+          type
+          siteContext {
+            path
+          }
+        }
+      }
+    }
+  }
+  ... on ContentPromotion {
+    linkText
+    linkUrl
+  }
+  ... on ContentContact {
+    title
+    phone
+    publicEmail
+  }
+  ... on ContentWebinar {
+    startDate
+  }
+  ... on ContentEvent {
+    startDate
+    endDate
+  }
+}
+
+`;
