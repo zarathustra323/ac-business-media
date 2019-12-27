ecca5c02bc5f3db6170b1abf0ee4230176b783de
Jacob Bare <jacob@limit0.io>
2019-12-27 12:20:50 -0600

    Add /sponsored-research whitepapers page

--- sites/sdcexec.com/server/routes/published-content.js
+++ sites/sdcexec.com/server/routes/published-content.js
@@ -1,8 +1,10 @@
 const webinars = require('@ac-business-media/refresh-theme/templates/published-content/webinars');
 const podcasts = require('@ac-business-media/refresh-theme/templates/published-content/podcasts');
+const whitepapers = require('@ac-business-media/refresh-theme/templates/published-content/whitepapers');
 
 module.exports = (app) => {
   app.get('/webinars', (_, res) => { res.marko(webinars); });
   // note: the `/podcast` route is intentional
   app.get('/podcast', (_, res) => { res.marko(podcasts); });
+  app.get('/sponsored-research', (_, res) => { res.marko(whitepapers, { title: 'Sponsored Research' }); });
 };
