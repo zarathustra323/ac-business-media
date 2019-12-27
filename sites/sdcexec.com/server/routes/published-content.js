164f0917ce9d94edd3b2303c41b3300bb55a59c7
Jacob Bare <jacob@limit0.io>
2019-12-27 12:29:45 -0600

    Add events published content page

--- sites/sdcexec.com/server/routes/published-content.js
+++ sites/sdcexec.com/server/routes/published-content.js
@@ -2,9 +2,11 @@ const webinars = require('@ac-business-media/refresh-theme/templates/published-c
 const podcasts = require('@ac-business-media/refresh-theme/templates/published-content/podcasts');
 const whitepapers = require('@ac-business-media/refresh-theme/templates/published-content/whitepapers');
 const blogs = require('@ac-business-media/refresh-theme/templates/published-content/blogs');
+const events = require('@ac-business-media/refresh-theme/templates/published-content/events');
 
 module.exports = (app) => {
   app.get('/blogs', (_, res) => { res.marko(blogs); });
+  app.get('/events', (_, res) => { res.marko(events); });
   app.get('/webinars', (_, res) => { res.marko(webinars); });
   // note: the `/podcast` route is intentional
   app.get('/podcast', (_, res) => { res.marko(podcasts); });
