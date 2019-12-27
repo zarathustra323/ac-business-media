89cb058e95d1705b84123fe3a6a5db0b44b938c5
Jacob Bare <jacob@limit0.io>
2019-12-27 12:23:48 -0600

    Add blogs published content page

--- sites/sdcexec.com/server/routes/published-content.js
+++ sites/sdcexec.com/server/routes/published-content.js
@@ -1,8 +1,10 @@
 const webinars = require('@ac-business-media/refresh-theme/templates/published-content/webinars');
 const podcasts = require('@ac-business-media/refresh-theme/templates/published-content/podcasts');
 const whitepapers = require('@ac-business-media/refresh-theme/templates/published-content/whitepapers');
+const blogs = require('@ac-business-media/refresh-theme/templates/published-content/blogs');
 
 module.exports = (app) => {
+  app.get('/blogs', (_, res) => { res.marko(blogs); });
   app.get('/webinars', (_, res) => { res.marko(webinars); });
   // note: the `/podcast` route is intentional
   app.get('/podcast', (_, res) => { res.marko(podcasts); });
