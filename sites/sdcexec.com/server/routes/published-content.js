2d66cf4c08c47f952ddbecc30f59672d7a7eff82
Jacob Bare <jacob@limit0.io>
2019-12-27 12:13:41 -0600

    Add /podcast published content route

--- sites/sdcexec.com/server/routes/published-content.js
+++ sites/sdcexec.com/server/routes/published-content.js
@@ -1,5 +1,8 @@
 const webinars = require('@ac-business-media/refresh-theme/templates/published-content/webinars');
+const podcasts = require('@ac-business-media/refresh-theme/templates/published-content/podcasts');
 
 module.exports = (app) => {
   app.get('/webinars', (_, res) => { res.marko(webinars); });
+  // note: the `/podcast` route is intentional
+  app.get('/podcast', (_, res) => { res.marko(podcasts); });
 };
