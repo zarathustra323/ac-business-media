973e885f6bf84530ad9acbaaf2f1d1a3d0ca75f5
Jacob Bare <jacob@limit0.io>
2019-12-27 12:59:28 -0600

    Add published content routes to FL

--- sites/foodlogistics.com/server/routes/index.js
+++ sites/foodlogistics.com/server/routes/index.js
@@ -2,6 +2,7 @@ const content = require('./content');
 const dynamicPages = require('./dynamic-page');
 const home = require('./home');
 const magazine = require('./magazine');
+const publishedContent = require('./published-content');
 const search = require('./search');
 const websiteSections = require('./website-section');
 
@@ -17,6 +18,9 @@ module.exports = (app) => {
 
   // Content Pages
   content(app);
+
+  // Published Content Pages
+  publishedContent(app);
 
   // Search
   search(app);
