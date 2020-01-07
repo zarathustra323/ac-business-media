e32a1bccb7793762fc1e3847335bd855673cec7b
Brian Miller <bmiller@southcomm.com>
2020-01-07 14:17:23 -0600

    remove busness as a primary section

--- sites/greenindustrypros.com/config/navigation.js
+++ sites/greenindustrypros.com/config/navigation.js
@@ -1,7 +1,6 @@
 module.exports = {
   primary: {
     items: [
-      { href: '/business', label: 'Business' },
       { href: '/lawn-maintenance', label: 'Lawn Maintenance' },
       { href: '/lawn-care-renovation', label: 'Lawn Care & Renovation' },
       { href: '/landscape-design-installation', label: 'Landscape Design & Installation' },
@@ -33,7 +32,6 @@ module.exports = {
     {
       label: 'Topics',
       items: [
-        { href: '/business', label: 'Business' },
         { href: '/lawn-maintenance', label: 'Lawn Maintenance' },
         { href: '/lawn-care-renovation', label: 'Lawn Care & Renovation' },
         { href: '/landscape-design-installation', label: 'Landscape Design & Installation' },
