87bc9b5b3cb2bd5a22476a5f276c25bd7bfbe195
Jacob Bare <jacob@limit0.io>
2020-01-07 11:34:10 -0600

    Add awards section route for fl and sdce

--- sites/foodlogistics.com/server/routes/website-section.js
+++ sites/foodlogistics.com/server/routes/website-section.js
@@ -1,11 +1,16 @@
 const { withWebsiteSection } = require('@base-cms/marko-web/middleware');
 const section = require('@ac-business-media/refresh-theme/templates/website-section');
+const awards = require('@ac-business-media/refresh-theme/templates/website-section/awards');
 const contactUs = require('@ac-business-media/refresh-theme/templates/website-section/contact-us');
 const queryFragment = require('@ac-business-media/refresh-theme/graphql/fragments/website-section-page');
 
 module.exports = (app) => {
   app.get('/:alias(contact-us)', withWebsiteSection({
     template: contactUs,
+    queryFragment,
+  }));
+  app.get('/:alias(awards)', withWebsiteSection({
+    template: awards,
     queryFragment,
   }));
   app.get('/:alias([a-z0-9-/]+)', withWebsiteSection({
