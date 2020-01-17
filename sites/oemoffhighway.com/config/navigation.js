d5b1092aea928e4be57062b53f5baa2d7923a51f
Jacob Bare <jacob@limit0.io>
2020-01-17 10:32:27 -0600

    Add secondary nav items and menu items

--- sites/oemoffhighway.com/config/navigation.js
+++ sites/oemoffhighway.com/config/navigation.js
@@ -12,19 +12,19 @@ module.exports = {
   },
   secondary: {
     items: [
+      { href: '/market-analysis', label: 'Market Analysis' },
+      { href: '/specguide', label: 'Spec Guides' },
+      { href: '/trends', label: 'Trends' },
+      { href: '/media-center', label: 'Media' },
+      { href: '/events', label: 'Events' },
+      { href: 'https://www.oemoffhighwaymediakit.com/', label: 'Advertise', target: '_blank' },
       { href: '/magazine', label: 'Magazine' },
-      { href: '/awards', label: 'Awards' },
-      { href: 'http://www.supplychainnetworkmediakit.com/', label: 'Advertise', target: '_blank' },
-      { href: '/contact-us', label: 'Contact Us' },
-      { href: '/webinars', label: 'Webinars' },
-      { href: 'http://www.supplychainlearningcenter.com/', label: 'Supply Chain Learning Center', target: '_blank' },
-      { href: '/sponsored-research', label: 'Sponsored Research' },
     ],
   },
   tertiary: {
     items: [
       {
-        href: 'https://acbm.dragonforms.com/loading.do?omedasite=SDCE_NL_signup',
+        href: 'https://acbusiness.dragonforms.com/loading.do?omedasite=ACBM_acctlookup&pub=ooh',
         label: 'Sign Up',
         icon: 'mail',
         forceLabel: true,
@@ -62,22 +62,19 @@ module.exports = {
     {
       label: 'Resources',
       items: [
-        { href: '/magazine', label: 'Magazines' },
-        { href: '/awards', label: 'Awards' },
-        { href: '/sponsored-research', label: 'Sponsored Research' },
+        { href: '/market-analysis', label: 'Market Analysis' },
+        { href: '/specguide', label: 'Spec Guides' },
+        { href: '/trends', label: 'Trends' },
+        { href: '/media-center', label: 'Media' },
         { href: '/events', label: 'Events' },
-        { href: '/webinars', label: 'Webinars' },
-        { href: 'http://www.supplychainlearningcenter.com/', label: 'Supply Chain Learning Center', target: '_blank' },
-        { href: '/podcast', label: 'Podcasts' },
-        { href: '/blogs', label: 'Expert Columnists' },
-        { href: '/multimedia', label: 'Multimedia' },
+        { href: '/magazine', label: 'Magazine' },
       ],
     },
     {
       label: 'User Tools',
       items: [
-        { href: 'ttps://acbm.dragonforms.com/loading.do?omedasite=SDCE_NL_signup', label: 'Newsletters', target: '_blank' },
-        { href: 'http://www.supplychainnetworkmediakit.com/', label: 'Advertise', target: '_blank' },
+        { href: 'https://acbusiness.dragonforms.com/loading.do?omedasite=ACBM_acctlookup&pub=ooh', label: 'Newsletters', target: '_blank' },
+        { href: 'https://www.oemoffhighwaymediakit.com/', label: 'Advertise', target: '_blank' },
         { href: '/contact-us', label: 'Contact Us' },
         { href: '/page/privacy-policy', label: 'Privacy Policy', target: '_blank' },
         { href: '/page/terms-conditions', label: 'Terms & Conditions', target: '_blank' },
