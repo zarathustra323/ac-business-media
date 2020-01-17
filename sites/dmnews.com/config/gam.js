859efbacdc22fb8da0b38893e9d5e1eb449ad98a
Jacob Bare <jacob@limit0.io>
2020-01-17 12:11:44 -0600

    Set legacy GAM ad unit paths

--- sites/dmnews.com/config/gam.js
+++ sites/dmnews.com/config/gam.js
@@ -1,18 +1,18 @@
 module.exports = {
-  basePath: 'OEMOff-Highway',
+  basePath: 'DMNews',
   // @todo these need to change once new adunits are created.
   pathMaps: [
     {
       alias: 'default',
       map: {
-        lb1: 'OOH_BS',
-        'lb-sticky-bottom': 'OOH_BS',
-        rail1: 'OOH_MR',
-        rail2: 'OOH_HP',
-        'infinite-rail': 'OOH_MR',
-        'infinite-interstitial': 'OOH_MR',
-        'in-content': 'OOH_MR',
-        reskin: 'OOH_Reskin',
+        lb1: 'DMN_BS',
+        'lb-sticky-bottom': 'DMN_BS',
+        rail1: 'DMN_MR',
+        rail2: 'DMN_HP',
+        'infinite-rail': 'DMN_MR',
+        'infinite-interstitial': 'DMN_MR',
+        'in-content': 'DMN_MR',
+        reskin: 'DMN_Reskin',
       },
     },
   ],
