72e060bce69e0bc0141967f861017940bc9d0df9
Brandon Krigbaum <brandonbk@gmail.com>
2020-01-22 15:38:35 -0600

    Add native-x config for GIP

---
+++ sites/greenindustrypros.com/config/native-x.js
@@ -0,0 +1,10 @@
+module.exports = {
+  placements: {
+    default: '5bed868f8e1d72000126d058',
+    'mowing-maintenance': '5bed845f8e1d72000126c340',
+    'lawn-care-renovation': '5bed81878e1d72000126b152',
+    'snow-ice': '5bed813a8e1d72000126aef6',
+    'design-installation': '5bed83fbf3eec100015e9180',
+    'irrigation-water': '5bed83c2f3eec100015e8fe4',
+  },
+};
