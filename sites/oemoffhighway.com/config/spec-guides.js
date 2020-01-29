89fd822bc20aa045d92ab29cabf2a7f4d8fb87fb
Jacob Bare <jacob@limit0.io>
2020-01-29 16:23:44 -0600

    Add spreadsheet src and column types

--- sites/oemoffhighway.com/config/spec-guides.js
+++ sites/oemoffhighway.com/config/spec-guides.js
@@ -3,6 +3,7 @@ module.exports = {
     title: '',
     image: '',
     pdfLink: '',
+    sheetSrc: 'https://spreadsheets.google.com/feeds/list/10EKomL-SVMZ4imnCaoO86t48T7ngVaopWXie04AveUo/4/public/values?alt=json',
     measures: {
       metric: { label: 'Metric' },
       standard: { label: 'Standard' },
@@ -12,25 +13,30 @@ module.exports = {
       hp: {
         measure: 'standard',
         range: ['hp-low', 'hp-high'],
+        type: 'number',
       },
       kw: {
         label: 'kW',
         measure: 'metric',
         range: ['kw-low', 'kw-high'],
+        type: 'number',
       },
       disp: {
-        label: 'Disp. (l)',
+        label: 'Disp. (L)',
         range: ['disp-low', 'disp-high'],
+        type: 'number',
       },
       torque: {
         label: 'Torque (lb.-ft.)',
         measure: 'standard',
         range: ['torque-low', 'torque-high'],
+        type: 'number',
       },
       torquenm: {
         label: 'Torque (Nm)',
         measure: 'metric',
         range: ['torque-lownm', 'torque-highnm'],
+        type: 'number',
       },
       fuel: {
         label: 'Fuel',
@@ -45,10 +51,12 @@ module.exports = {
       weightdrylb: {
         label: 'Dry Weight (lb.)',
         measure: 'standard',
+        type: 'number',
       },
       weightdrykg: {
         label: 'Dry Weight (kg.)',
         measure: 'metric',
+        type: 'number',
       },
       model: {
         label: 'Model',
