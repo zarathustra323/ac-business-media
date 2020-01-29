9e8e6734c58f8514e4a6e4acac30dc6116365760
Jacob Bare <jacob@limit0.io>
2020-01-29 11:52:01 -0600

    Add additional fields

--- sites/oemoffhighway.com/config/spec-guides.js
+++ sites/oemoffhighway.com/config/spec-guides.js
@@ -7,7 +7,7 @@ module.exports = {
       metric: { label: 'Metric' },
       standard: { label: 'Standard' },
     },
-    initialMeasureKey: 'metric',
+    initialMeasureKey: 'standard',
     columns: {
       hp: {
         measure: 'standard',
@@ -18,8 +18,46 @@ module.exports = {
         measure: 'metric',
         range: ['kw-low', 'kw-high'],
       },
+      disp: {
+        label: 'Disp. (l)',
+        range: ['disp-low', 'disp-high'],
+      },
+      torque: {
+        label: 'Torque (lb.-ft.)',
+        measure: 'standard',
+        range: ['torque-low', 'torque-high'],
+      },
+      torquenm: {
+        label: 'Torque (Nm)',
+        measure: 'metric',
+        range: ['torque-lownm', 'torque-highnm'],
+      },
       fuel: {
         label: 'Fuel',
+      },
+      tierregcompliant: {
+        label: 'Emissions Certifications',
+      },
+      aftertreatment: {
+        label: 'Aftertreatment',
+      },
+      // Dimension group here.
+      weightdrylb: {
+        label: 'Dry Weight (lb.)',
+        measure: 'standard',
+      },
+      weightdrykg: {
+        label: 'Dry Weight (kg.)',
+        measure: 'metric',
+      },
+      model: {
+        label: 'Model',
+      },
+      familyseries: {
+        label: 'Family / Series',
+      },
+      company: {
+        label: 'Company',
       },
     },
   },
