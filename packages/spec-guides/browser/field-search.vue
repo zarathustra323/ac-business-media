579032610227a7e03ec1055c6a9a5dc1251c8b27
Jacob Bare <jacob@limit0.io>
2020-01-29 14:39:50 -0600

    Create field search component

---
+++ packages/spec-guides/browser/field-search.vue
@@ -0,0 +1,55 @@
+<template>
+  <div v-if="columns.length">
+    <select :disabled="disabled" @change="emitFieldChange">
+      <option
+        v-for="col in columns"
+        :key="col.key"
+        :value="col.key"
+        :selected="col.key === selected"
+      >
+        {{ col.label }}
+      </option>
+    </select>
+    <input
+      :value="phrase"
+      :disabled="disabled"
+      type="text"
+      placeholder="Enter search value..."
+      @input="emitPhraseChange(...arguments)"
+    >
+  </div>
+</template>
+
+<script>
+import { debounce } from 'debounce';
+
+export default {
+  props: {
+    phrase: {
+      type: String,
+      default: null,
+    },
+    selected: {
+      type: String,
+      default: null,
+    },
+    columns: {
+      type: Array,
+      default: () => [],
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+
+  methods: {
+    emitFieldChange(event) {
+      this.$emit('field-change', event);
+    },
+    emitPhraseChange: debounce(function emit(event) {
+      this.$emit('phrase-change', event);
+    }, 250),
+  },
+};
+</script>
