f9912ae6bec3ea10eab3d363fb4564b86a4c69ae
Jacob Bare <jacob@limit0.io>
2020-01-31 08:09:23 -0600

    Add description and bullets for motor & pump

--- sites/oemoffhighway.com/config/spec-guides/motor.js
+++ sites/oemoffhighway.com/config/spec-guides/motor.js
@@ -1,6 +1,11 @@
 module.exports = {
   title: 'Motor Spec Guide',
   teaser: 'Over 500 fluid power motors ranging from fixed gear to bent axis are waiting to be sorted through to find the exact motor to fit your equipment design requirements.',
+  description: 'The <strong><em>OEM Off-Highway Motor Spec Guide</em></strong> is the industry’s first online, interactive component specing tool custom-built for engineers and product development team members to find exactly which motor they need for integration into the next generation of equipment.',
+  bullets: [
+    'Entering specific search criteria (Company Name, Pressure, Speed, etc.)',
+    'Sorting the information by any column to order the information the way you need it',
+  ],
   image: {
     src: 'https://img.oemoffhighway.com/files/base/acbm/ooh/image/static/motor.png',
   },
--- sites/oemoffhighway.com/config/spec-guides/pump.js
+++ sites/oemoffhighway.com/config/spec-guides/pump.js
@@ -1,6 +1,11 @@
 module.exports = {
   title: 'Pump Spec Guide',
   teaser: 'Search through over 500 fluid power pump products to find the unit that suits your application, from gear pumps to axial piston pumps.',
+  description: 'The <strong><em>OEM Off-Highway Pump Spec Guide</em></strong> is the industry’s first online, interactive component specing tool custom-built for engineers and product development team members to find exactly which pump they need for integration into the next generation of equipment.',
+  bullets: [
+    'Entering specific search criteria (Company Name, Pressure, Speed, etc.)',
+    'Sorting the information by any column to order the information the way you need it',
+  ],
   image: {
     src: 'https://img.oemoffhighway.com/files/base/acbm/ooh/image/static/pump.png',
   },
