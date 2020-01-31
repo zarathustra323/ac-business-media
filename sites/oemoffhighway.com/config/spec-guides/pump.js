6b633362abc10256221e760e2283557116ae6af8
Jacob Bare <jacob@limit0.io>
2020-01-31 08:44:44 -0600

    Configure pump

--- sites/oemoffhighway.com/config/spec-guides/pump.js
+++ sites/oemoffhighway.com/config/spec-guides/pump.js
@@ -10,4 +10,100 @@ module.exports = {
     src: 'https://img.oemoffhighway.com/files/base/acbm/ooh/image/static/pump.png',
   },
   downloadLink: '/20839221',
+  sheetSrc: 'https://spreadsheets.google.com/feeds/list/10EKomL-SVMZ4imnCaoO86t48T7ngVaopWXie04AveUo/5/public/values?alt=json',
+  measures: {
+    metric: { label: 'Metric' },
+    standard: { label: 'Standard' },
+  },
+  initialMeasureKey: 'metric',
+  columns: {
+    openloop: {
+      label: 'Open Loop',
+    },
+    closedloop: {
+      label: 'Closed Loop',
+    },
+    unit: {
+      label: 'Unit',
+    },
+    type: {
+      label: 'Type',
+    },
+    in3rev: {
+      label: 'Disp. (in3/rev)',
+      measure: 'standard',
+      range: ['in3rev-low', 'in3rev-high'],
+      type: 'number',
+    },
+    cm3rev: {
+      label: 'Disp. (cm3/rev)',
+      measure: 'metric',
+      range: ['cm3rev-low', 'cm3rev-high'],
+      type: 'number',
+    },
+    'nominal-bar': {
+      label: 'Nominal Press (bar)',
+      measure: 'metric',
+      range: ['nominal-lowbar', 'nominal-highbar'],
+      type: 'number',
+    },
+    'peak-bar': {
+      label: 'Peak Press (bar)',
+      measure: 'metric',
+      range: ['peak-lowbar', 'peak-highbar'],
+      type: 'number',
+    },
+    'nominal-psi': {
+      label: 'Nominal Press (psi)',
+      measure: 'standard',
+      range: ['nominal-lowpsi', 'nominal-highpsi'],
+      type: 'number',
+    },
+    'peak-psi': {
+      label: 'Peak Press (psi)',
+      measure: 'standard',
+      range: ['peak-lowpsi', 'peak-highpsi'],
+      type: 'number',
+    },
+    continuous: {
+      label: 'Cont. Speed (RPM)',
+      range: ['continuous-low', 'continuous-high'],
+      type: 'number',
+    },
+    max: {
+      label: 'Max Speed (RPM)',
+      range: ['max-low', 'max-high'],
+      type: 'number',
+    },
+    hp: {
+      label: 'Max Input (hp)',
+      measure: 'standard',
+      range: ['hp-low', 'hp-high'],
+      type: 'number',
+    },
+    kw: {
+      label: 'Max Input (kW)',
+      measure: 'metric',
+      range: ['kw-low', 'kw-high'],
+      type: 'number',
+    },
+    lpm: {
+      label: 'Thoe. Flow at Rated Speed (lpm)',
+      measure: 'metric',
+      range: ['lpm-low', 'lpm-high'],
+      type: 'number',
+    },
+    gpm: {
+      label: 'Thoe. Flow at Rated Speed (gpm)',
+      measure: 'standard',
+      range: ['gpm-low', 'gpm-high'],
+      type: 'number',
+    },
+    productseries: {
+      label: 'Product / Series',
+    },
+    company: {
+      label: 'Company',
+    },
+  },
 };
