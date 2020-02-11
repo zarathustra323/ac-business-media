0ad9464d569a12bf83854bf15e1e18de709dec42
Brian Miller <bmiller@southcomm.com>
2020-02-11 14:46:19 -0600

    Change page alias lookup to support all characters
    Previously it would only match alpha numerical characters and -

--- sites/foodlogistics.com/server/routes/dynamic-page.js
+++ sites/foodlogistics.com/server/routes/dynamic-page.js
@@ -3,7 +3,7 @@ const page = require('@ac-business-media/refresh-theme/templates/dynamic-page');
 const queryFragment = require('@ac-business-media/refresh-theme/graphql/fragments/dynamic-page');
 
 module.exports = (app) => {
-  app.get('/page/:alias', withDynamicPage({
+  app.get('/page/:alias(*)', withDynamicPage({
     template: page,
     queryFragment,
   }));
