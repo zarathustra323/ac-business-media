622c548ef6eff7fd66fb4e7fcdeb35f5c7287c98
Josh Worden <solocommand@gmail.com>
2020-03-03 13:01:11 -0600

    Update navigation

--- sites/sdcexec.com/config/navigation.js
+++ sites/sdcexec.com/config/navigation.js
@@ -15,6 +15,8 @@ module.exports = {
     items: [
       { href: '/magazine', label: 'Magazine' },
       { href: '/awards', label: 'Awards' },
+      { href: '/events', label: 'Events' },
+      { href: '/blogs', label: 'Expert Columnists' },
       { href: 'http://www.supplychainnetworkmediakit.com/', label: 'Advertise', target: '_blank' },
       { href: '/contact-us', label: 'Contact Us' },
       { href: '/webinars', label: 'Webinars' },
