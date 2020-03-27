4b370c72a79e31d4a5fa5b3b2787c50e7ac6e5f9
Josh Worden <solocommand@gmail.com>
2020-03-27 13:46:26 -0500

    Remove unused templates from OEM

--- sites/forconstructionpros.com/server/routes/website-section.js
+++ sites/forconstructionpros.com/server/routes/website-section.js
@@ -2,15 +2,11 @@ const { withWebsiteSection } = require('@base-cms/marko-web/middleware');
 const section = require('@ac-business-media/refresh-theme/templates/website-section');
 const contactUs = require('@ac-business-media/refresh-theme/templates/website-section/contact-us');
 const events = require('@ac-business-media/refresh-theme/templates/website-section/events');
-const feed = require('@ac-business-media/refresh-theme/templates/website-section/feed');
-const mediaGalleries = require('@ac-business-media/refresh-theme/templates/website-section/media-galleries');
 const webinars = require('@ac-business-media/refresh-theme/templates/website-section/webinars');
-const whitepapers = require('@ac-business-media/refresh-theme/templates/website-section/whitepapers');
 const queryFragment = require('@ac-business-media/refresh-theme/graphql/fragments/website-section-page');
 
 const economics = require('../templates/website-section/economics');
 const directory = require('../templates/website-section/directory');
-const mediaCenter = require('../templates/website-section/media-center');
 
 module.exports = (app) => {
   app.get('/:alias(contact-us)', withWebsiteSection({
@@ -35,26 +31,6 @@ module.exports = (app) => {
   }));
   app.get('/:alias(directory/[a-z0-9-/]+)', withWebsiteSection({
     template: directory,
-    queryFragment,
-  }));
-  app.get('/:alias(media-center)', withWebsiteSection({
-    template: mediaCenter,
-    queryFragment,
-  }));
-  app.get('/:alias(media-center/photo-galleries)', withWebsiteSection({
-    template: mediaGalleries,
-    queryFragment,
-  }));
-  app.get('/:alias(media-center/whitepapers-ebooks)', withWebsiteSection({
-    template: whitepapers,
-    queryFragment,
-  }));
-  app.get('/:alias(media-center/videos/industry-videos)', withWebsiteSection({
-    template: feed,
-    queryFragment,
-  }));
-  app.get('/:alias(media-center/videos/technology-news-tracker)', withWebsiteSection({
-    template: feed,
     queryFragment,
   }));
   app.get('/:alias([a-z0-9-/]+)', withWebsiteSection({
