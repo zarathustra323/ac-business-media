ccb8879b9a8c860b9e7c3ae9257c0692753f7165
Josh Worden <solocommand@gmail.com>
2020-04-01 16:21:03 -0500

    Add directory package to FL

---
+++ sites/foodlogistics.com/config/categories.js
@@ -0,0 +1,47 @@
+module.exports = [
+  {
+    id: 57736,
+    name: '3PL/4PL',
+    href: '/3pl-4pl',
+  },
+  {
+    id: 57735,
+    name: 'Cold Chain',
+    href: '/cold-chain',
+  },
+  {
+    id: 57738,
+    name: 'Ocean Ports & Carriers',
+    href: '/ocean-ports-carriers',
+  },
+  {
+    id: 57737,
+    name: 'Risk/Compliance',
+    href: '/risk-compliance',
+  },
+  {
+    id: 57733,
+    name: 'Safety & Security',
+    href: '/safety',
+  },
+  {
+    id: 57732,
+    name: 'Software & Technology',
+    href: '/technology',
+  },
+  {
+    id: 57734,
+    name: 'Sustainability',
+    href: '/sustainability',
+  },
+  {
+    id: 57731,
+    name: 'Transportation',
+    href: '/transportation',
+  },
+  {
+    id: 57730,
+    name: 'Warehousing',
+    href: '/warehousing',
+  },
+];
