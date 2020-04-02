4ceda5ad038bfa211805905c58265a28b26b6f53
Josh Worden <solocommand@gmail.com>
2020-04-02 10:38:01 -0500

    Add directory to SDC

---
+++ sites/sdcexec.com/config/categories.js
@@ -0,0 +1,42 @@
+module.exports = [
+  {
+    id: 57720,
+    name: '3PL/4PL',
+    href: '/3pl-4pl',
+  },
+  {
+    id: 57719,
+    name: 'Professional Development',
+    href: '/professional-development',
+  },
+  {
+    id: 57715,
+    name: 'Risk/Compliance',
+    href: '/risk-compliance',
+  },
+  {
+    id: 57716,
+    name: 'Software & Technology',
+    href: '/software-technology',
+  },
+  {
+    id: 57714,
+    name: 'Sourcing/Procurement',
+    href: '/sourcing-procurement',
+  },
+  {
+    id: 57718,
+    name: 'Sustainability',
+    href: '/sustainability',
+  },
+  {
+    id: 57713,
+    name: 'Transportation',
+    href: '/transportation',
+  },
+  {
+    id: 57712,
+    name: 'Warehousing',
+    href: '/warehousing',
+  },
+];
