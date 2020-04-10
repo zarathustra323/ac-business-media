e9dcc4bac3c01bda6117a8a12948973d42846f62
Jacob Bare <jacob@limit0.io>
2020-04-10 13:29:07 -0500

    Delete unused inquiry package

--- packages/inquiry/browser/index.js
+++
@@ -1,6 +0,0 @@
-import DefaultForm from './default-form.vue';
-
-export default (Browser, { component, mountPoint } = {}) => {
-  const InquiryForm = component || DefaultForm;
-  Browser.registerComponent('InquiryForm', InquiryForm, { mountPoint });
-};
