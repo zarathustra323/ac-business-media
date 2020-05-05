bcad04fd0334e17b7181b9c55ec10bcb3bd99909
Jacob Bare <jacob@limit0.io>
2020-05-05 08:26:09 -0500

    Fix native-x placements for sdce

--- sites/sdcexec.com/config/native-x.js
+++ sites/sdcexec.com/config/native-x.js
@@ -2,10 +2,10 @@ module.exports = {
   placements: {
     default: '5b560a22a4ac010001bbc500',
     warehousing: '5b560978a4ac010001bbc4f9',
-    transport: '5b5609832a488f0001ecf2c3',
-    procurement: '5b56098f2a488f0001ecf2c5',
+    transportation: '5b5609832a488f0001ecf2c3',
+    'sourcing-procurement': '5b56098f2a488f0001ecf2c5',
     'risk-compliance': '5b5609a62a488f0001ecf2c7',
-    technology: '5b5609b52a488f0001ecf2c8',
+    'software-technology': '5b5609b52a488f0001ecf2c8',
     sustainability: '5b5609c6a4ac010001bbc4fc',
     '3pl-4pl': '5b5609e22a488f0001ecf2c9',
     'professional-development': '5b5609ee2a488f0001ecf2ca',
