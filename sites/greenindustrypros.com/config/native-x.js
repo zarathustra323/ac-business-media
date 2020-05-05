c698f70d526d50f406b1386a45615130006da96c
Jacob Bare <jacob@limit0.io>
2020-05-05 08:25:38 -0500

    Fix native-x placements for gip

--- sites/greenindustrypros.com/config/native-x.js
+++ sites/greenindustrypros.com/config/native-x.js
@@ -3,8 +3,8 @@ module.exports = {
     default: '5bed868f8e1d72000126d058',
     'mowing-maintenance': '5bed845f8e1d72000126c340',
     'lawn-care-renovation': '5bed81878e1d72000126b152',
-    'snow-ice': '5bed813a8e1d72000126aef6',
+    'snow-ice-management': '5bed813a8e1d72000126aef6',
     'design-installation': '5bed83fbf3eec100015e9180',
-    'irrigation-water': '5bed83c2f3eec100015e8fe4',
+    'irrigation-water-management': '5bed83c2f3eec100015e8fe4',
   },
 };
