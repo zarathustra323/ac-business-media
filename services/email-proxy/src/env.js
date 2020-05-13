3ef8948ec4a535fae3ed42f21e29a058d62a4153
Jacob Bare <jacob@limit0.io>
2020-05-13 14:49:17 -0500

    Add terminus to email proxy service

--- services/email-proxy/src/env.js
+++ services/email-proxy/src/env.js
@@ -1,9 +1,12 @@
 const {
   cleanEnv,
+  num,
   port,
 } = require('@base-cms/env');
 
 module.exports = cleanEnv(process.env, {
   EXPOSED_PORT: port({ desc: 'The external port that express is exposed on.', default: 80 }),
   PORT: port({ desc: 'The internal port that express will run on.', default: 80 }),
+  TERMINUS_TIMEOUT: num({ desc: 'Number of milliseconds before forceful exiting.', default: 1000 }),
+  TERMINUS_SHUTDOWN_DELAY: num({ desc: 'Number of milliseconds before the HTTP server starts its shutdown.', default: 10000 }),
 });
