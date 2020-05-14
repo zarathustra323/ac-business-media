3a3eef358ccace1488d235a38352956804ffaf8e
Jacob Bare <jacob@limit0.io>
2020-05-14 13:56:42 -0500

    Process email-x ad links

---
+++ services/email-proxy/src/hooks/process-omeda-links/process-emailx-ads.js
@@ -0,0 +1,28 @@
+const fetch = require('node-fetch');
+const { EMAILX_HOSTNAME } = require('../../env');
+
+const pattern = new RegExp(`https://${EMAILX_HOSTNAME}/click/[a-f0-9]{24}.*`, 'i');
+
+module.exports = async ({ html, urls = [] }) => {
+  const appendedUrls = await Promise.all(urls.map(async (url) => {
+    const match = pattern.exec(url.value);
+    if (match && match[0]) {
+      const dataUrl = match[0].replace(/\/click\//, '/data/');
+      const res = await fetch(dataUrl);
+      if (res.status === 200) {
+        const adData = await res.json();
+        return { ...url, adData };
+      }
+    }
+    return url;
+  }));
+
+
+  let replaced = html;
+  appendedUrls.filter(url => url.adData).forEach((url) => {
+    const { element, adData } = url;
+    const dpm = `<!--DPM: ln="${adData.ad.name}" lc="Advertising" lcv="${adData.ad.advertiserName}" -->`;
+    replaced = replaced.replace(element, `${element}${dpm}`);
+  });
+  return replaced;
+};
